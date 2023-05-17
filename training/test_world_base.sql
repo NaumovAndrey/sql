5077e0a407ef22871e33bbd1d154b1722187092e
andrey <naumov.a.n@icloud.com>
2023-05-17 22:18:51 +0700

    Тренировка с базой world от MySql

--- training/test_world_base.sql
+++ training/test_world_base.sql
@@ -1,7 +1,13 @@
+USE world;
+
 SELECT * FROM city;
+
+SELECT * FROM country;
 
 DESC city;
 
+
+#уникальность distinct
 SELECT DISTINCT Continent AS Континенты, Population AS Население
 FROM country;
 
@@ -12,4 +18,38 @@ LIMIT 15;
 
 SELECT DISTINCT Code, Continent AS Континенты, Population AS Население
 FROM country
-WHERE Code = 'RUS'
+WHERE Code = 'RUS';
+
+SELECT Code, LEFT(Name, 15), Population
+FROM country
+WHERE SurfaceArea
+    BETWEEN 1000000 AND 4000000;
+
+# Выборка в таблице city в колонке name окончание на ul (like - поиск)
+SELECT Name, Population
+FROM city
+WHERE  Name LIKE 'novo%';
+
+# Информация о населении
+SELECT Name, Population
+FROM city
+WHERE Name IN ('Moscow', 'Novosibirsk', 'Barnaul');
+
+#Сортировка по name.
+SELECT Name, Population
+FROM city
+ORDER BY Name
+LIMIT 20;
+
+# Сортировка с конца столбца desc.
+SELECT Name, Population
+FROM city
+ORDER BY Name DESC
+LIMIT 20;
+
+# Сортировка random
+SELECT Name, Population
+FROM city
+ORDER BY rand()
+LIMIT 20;
+
