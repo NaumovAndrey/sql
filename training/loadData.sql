d4292a69fd1e9bf062999770d46c1fbfaba7ce38
andrey <naumov.a.n@icloud.com>
2023-05-21 15:16:11 +0700

    Загрузка данных из txt файла

--- training/loadData.sql
+++ training/loadData.sql
@@ -1,11 +1,14 @@
 SHOW CREATE TABLE table1;
+USE test_database;
 
 SHOW VARIABLES LIKE 'secure_file_priv';
 
 LOAD DATA
-    INFILE 'E:\mysql\query2.txt'
+    INFILE 'E:\\mysql\\query2.txt'
     INTO TABLE table1
     FIELDS TERMINATED BY ','
     LINES TERMINATED BY '\n';
 
 SELECT * FROM table1;
+
+DESC table1;
